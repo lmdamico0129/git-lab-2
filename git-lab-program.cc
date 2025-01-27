e9df4e4342a75bb6c7bba715692443473ce192ec
Lauren Damico <ld078522@ohio.edu>
2025-01-27 14:11:06 -0500

    New changes

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Lauren Damico
+ *        Date: Jan 27 2025
+ * Description: Test code to push into repository for CS 2400 Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
